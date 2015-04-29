abdd0e60c6eb1f59aa98b83e3a3f8f6b8fc1bf8f
Tomasz Dziuda <dziudek@gmail.com>
2015-04-29 11:51:15 +0200

    Fixed issue with missing images

--- content-header.php
+++ content-header.php
@@ -18,7 +18,7 @@
 	)
 ) : ?>
 <header class="entry-header<?php if(is_singular() && is_sticky()) :?> sticky<?php endif; ?><?php if(get_theme_mod('portfolio_full_width_images', '1') == '1') : ?> full-width-image<?php endif; ?><?php if(get_theme_mod('portfolio_post_show_title', '1') == '0') : ?> no-title<?php endif; ?>" data-url="<?php echo get_permalink(); ?>">
-	<?php if(get_theme_mod('portfolio_post_show_featured_image', '1') == '1') : ?>
+	<?php if(!is_singular() || get_theme_mod('portfolio_post_show_featured_image', '1') == '1') : ?>
 		<?php if (has_post_thumbnail() && !post_password_required()) : ?>			
 			<?php the_post_thumbnail(is_singular() ? 'full' : 'gk-portfolio-size'); ?>
 		<?php elseif($video_code) : ?>
